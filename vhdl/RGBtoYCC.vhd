c439706de1dde1d3ae21d11bf55ef6a8b3292c80
Tony Bashi <bashia@ugls15.ece.uvic.ca>
2014-07-30 23:27:36 -0700

    Revised RGBtoYCC.vhd

--- vhdl/RGBtoYCC.vhd
+++ vhdl/RGBtoYCC.vhd
@@ -22,19 +22,19 @@ end RGBtoYCC;
 
 
 architecture Behavioral of RGBtoYCC is
-	constant yRConst : integer := 2498396;
-	constant yGConst : integer := 4904878;
-	constant yBConst : integer := 952566;
+	constant yRConst : std_logic_vector := x"261F5C";
+	constant yGConst : std_logic_vector := x"4AD7AE";
+	constant yBConst : std_logic_vector := x"E88F6";
 
-	constant cbRConst : integer := 2498407;
-	constant cbGConstA : integer := 4904867;
-	constant cbGConstB : integer := 1437774;
-	constant cbBConst : integer := 7403274;
+	constant cbRConst : std_logic_vector := x"261F67";
+	constant cbGConstA : std_logic_vector := x"4AD7A3";
+	constant cbGConstB : std_logic_vector := x"15F04E";
+	constant cbBConst : std_logic_vector := x"70F70A";
 
-	constant crRConst : integer := 5857443;
-	constant crGConstA : integer := 4904877;
-	constant crGConstB : integer := 4272902;
-	constant crBConst : integer := 952567;
+	constant crRConst : std_logic_vector := x"5960A3";
+	constant crGConstA : std_logic_vector := x"4AD7AD";
+	constant crGConstB : std_logic_vector := x"413306";
+	constant crBConst : std_logic_vector := x"E88F7";
 
     signal r,g,b,y,cr,cb: UNSIGNED (7 downto 0);
 begin
@@ -42,9 +42,9 @@ begin
 	g <= unsigned(inport(15 downto 8));
 	b <= unsigned(inport(7 downto 0));
 
-   y <= shift_right((yRConst * r + yGConst * g + yBConst * b) ,23) + 16;
-   cb <= shift_right((b * cbBConst - r * cbRConst - g * cbGConstA), 24);
-   cr <= shift_right((crRConst * r - crGConstA * g - crBConst * b), 24);
+   y <= shift_right((unsigned(yRConst) * r + unsigned(yGConst) * g + unsigned(yBConst) * b) ,23) + 16;
+   cb <= shift_right((b * unsigned(cbBConst) - r * unsigned(cbRConst) - g * unsigned(cbGConstA)), 24);
+   cr <= shift_right((unsigned(crRConst) * r - unsigned(crGConstA) * g - unsigned(crBConst) * b), 24);
 	
 	outport(23 downto 16) <= std_logic_vector(y);
 	outport(15 downto 8) <= std_logic_vector(cb);
