da44ea3df0060cfb75d30b4fab3ca57e5db52a3b
bashia <bashia@uvic.ca>
2014-07-31 16:43:22 -0700

    Implemented Evan's changes.

--- vhdl/RGBtoYCC.vhd
+++ vhdl/RGBtoYCC.vhd
@@ -1,5 +1,3 @@
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -22,40 +20,40 @@ end RGBtoYCC;
 
 
 architecture Behavioral of RGBtoYCC is
-	constant yRConst : std_logic_vector := x"261F5C";
-	constant yGConst : std_logic_vector := x"4AD7AE";
-	constant yBConst : std_logic_vector := x"E88F6";
+	signal yRConst : std_logic_vector := x"261F5C";
+	signal yGConst : std_logic_vector := x"4AD7AE";
+	signal yBConst : std_logic_vector := x"E88F6";
 
-	constant cbRConst : std_logic_vector := x"261F67";
-	constant cbGConstA : std_logic_vector := x"4AD7A3";
-	constant cbGConstB : std_logic_vector := x"15F04E";
-	constant cbBConst : std_logic_vector := x"70F70A";
+	signal cbRConst : std_logic_vector := x"261F67";
+	signal cbGConstA : std_logic_vector := x"4AD7A3";
+	signal cbGConstB : std_logic_vector := x"15F04E";
+	signal cbBConst : std_logic_vector := x"70F70A";
 
-	constant crRConst : std_logic_vector := x"5960A3";
-	constant crGConstA : std_logic_vector := x"4AD7AD";
-	constant crGConstB : std_logic_vector := x"413306";
-	constant crBConst : std_logic_vector := x"E88F7";
+	signal crRConst : std_logic_vector := x"5960A3";
+	signal crGConstA : std_logic_vector := x"4AD7AD";
+	signal crGConstB : std_logic_vector := x"413306";
+	signal crBConst : std_logic_vector := x"E88F7";
 
-    signal r,g,b,y,cr,cb: UNSIGNED (7 downto 0);
+    signal r,g,b: UNSIGNED (7 downto 0);
+    signal y,cr,cb: UNSIGNED (31 downto 0);
+    signal tempy,tempcr,tempcb: std_logic_vector(31 downto 0);
 begin
 	r <= unsigned(inport(23 downto 16));
 	g <= unsigned(inport(15 downto 8));
 	b <= unsigned(inport(7 downto 0));	
 
-   y <= shift_right((unsigned(yRConst) * r + unsigned(yGConst) * g + unsigned(yBConst) * b) ,23) + 16;
-   cb <= shift_right((b * unsigned(cbBConst) - r * unsigned(cbRConst) - g * unsigned(cbGConstA)), 24);
-   cr <= shift_right((unsigned(crRConst) * r - unsigned(crGConstA) * g - unsigned(crBConst) * b), 24);
-	
 
-    process(clk)
-    begin
 
-	
-	outport(31 downto 24) <= x"00";
-	outport(23 downto 16) <= std_logic_vector(y);
-	outport(15 downto 8) <= std_logic_vector(cb);
-	outport(7 downto 0) <= std_logic_vector(cr);
-         
-    end process;
+   y <= (unsigned(yRConst) * r) + (unsigned(yGConst) * g) + (unsigned(yBConst) * b) + 16; --23
+   tempy <=std_logic_vector(y);
+   cb <= (b * unsigned(cbBConst) - r * unsigned(cbRConst) - g * unsigned(cbGConstA)); --24
+   tempcb <= std_logic_vector(cb);
+   cr <= (unsigned(crRConst) * r) - (unsigned(crGConstA) * g) - (unsigned(crBConst) * b); --24
+   tempcr <= std_logic_vector(cr);
+   
+   outport(31 downto 24) <= x"00";
+   outport(23 downto 16) <= tempy(30 downto 23);
+   outport(15 downto 8) <= tempcb(30 downto 23);
+   outport(7 downto 0) <= tempcb(30 downto 23);
 
 end Behavioral;
